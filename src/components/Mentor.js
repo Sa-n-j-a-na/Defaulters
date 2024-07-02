8bb54b25b4cdff7d25fc3cc02270814f71ea77ab
unknown <sanjana0411297@gmail.com>
2024-07-02 09:19:34 +0530

    nav

--- src/components/Mentor.js
+++ src/components/Mentor.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import styles from './comp.css';
-
+import './comp.css';
 
 function Mentor() {
   const location = useLocation();
-  const { username } = location.state;
+  const { username } = location.state || { username: 'MENTOR User' };
 
   return (
-    <div className={styles.compContainer}>
-      <h1>Welcome, {username} (MENTOR)</h1>
+    <div className="compContainer">
+      <div className="compHeader">
+        <img src="/image.png" alt="VCET Logo" />
+        <h1>VELAMMAL COLLEGE OF ENGINEERING AND TECHNOLOGY</h1>
+        <a href="/">Sign Out</a>
+      </div>
+      <div className="contentWrapper">
+        <div className="sidebar">
+          <h2>Menus</h2>
+          <a href="/">Home</a>
+          <a href="/attendance">Generate Report</a>
+        </div>
+        <div className="mainContent">
+          <div className="welcome">
+            Welcome "{username}" (MENTOR)
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
